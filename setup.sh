419b3b1d9cd2384e57a90037175d4f51196d119b
KM <modzelewski.kamil@gmail.com>
2020-10-16 19:45:09 +0200

    standard commit

--- setup.sh
+++ setup.sh
@@ -11,17 +11,6 @@ function clone_vim()
 	git clone https://github.com/kmmod/vim.git "${HOME}/.vim" \
 		|| die "Could not clone repository to ${HOME}/.vim"
   
-	git init "${HOME}/.vim/pack/"	
-
-  # PLUGINS 
-
-  git submodule add https://github.com/scrooloose/nerdtree "${HOME}/.vim/pack/plugins/start/nerdtree"
-  git submodule add https://github.com/Xuyuanp/nerdtree-git-plugin "${HOME}/.vim/pack/plugins/start/nerdtree-git-plugin"
-
-  # SCHEMES
-
-  git submodule add https://github.com/rakr/vim-one.git "${HOME}/.vim/pack/colors/start/one"
-
 }
 
 clone_vim
